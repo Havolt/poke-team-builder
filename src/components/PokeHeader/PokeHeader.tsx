f05d142f49e158c76c51626871c75c960371609b
Mark <markfitz815@gmail.com>
2026-02-22 21:05:21 +0000

    Add header

---
+++ src/components/PokeHeader/PokeHeader.tsx
@@ -0,0 +1,11 @@
+import Styles from "./PokeHeader.module.css";
+
+function PokeHeader() {
+  return (
+    <div className={Styles["pk-header"]}>
+      <h1>Poke Team Builder</h1>
+    </div>
+  );
+}
+
+export default PokeHeader;
