823d1eb3c78c6bf8acaacaec14a5237bfb4e69d3
Mark <markfitz815@gmail.com>
2026-02-23 21:33:46 +0000

    Grab 151 pokemon data, store as JSON

--- src/components/PokePicker/PokePicker.tsx
+++ src/components/PokePicker/PokePicker.tsx
@@ -1,9 +1,31 @@
 import { useEffect } from "react";
 
+const get151Pokemon = async (controller: AbortController) => {
+  try {
+    const response = await fetch(
+      "https://pokeapi.co/api/v2/pokemon?limit=151",
+      {
+        signal: controller.signal,
+      },
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error fetching Pokémon data:", error);
+  }
+};
+
 function PokePicker() {
   useEffect(() => {
     console.log("PokePicker mounted");
+
+    const controller = new AbortController();
+
+    const fetchPromise = get151Pokemon(controller);
+
     return () => {
+      // This cancels the first request when React "re-mounts"
+      controller.abort();
       console.log("PokePicker unmounted");
     };
   }, []);
