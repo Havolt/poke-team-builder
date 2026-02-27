9c773cfe344a972b003563895b6f9e124ac88805
Mark <markfitz815@gmail.com>
2026-02-27 20:53:26 +0000

    Fetch bulbasaur as example

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import PokeHeader from "./components/PokeHeader";
 import PokePicker from "./components/PokePicker";
 import PokeParty from "./components/PokeParty";
 
-import { useState } from "react";
+import links from "./constants/links.json";
+
+import { useState, useRef, useEffect } from "react";
 
 import "./App.css";
 
@@ -10,9 +12,32 @@ function App() {
   const [selectedPokemon, setSelectedPokemon] = useState<number | null>(null);
   const [userParty, setUserParty] = useState<number[]>([]);
 
+  const pokedex = useRef<{ [key: number]: any }>({}).current;
+
   const handleUnselectPokemon = () => {
     setSelectedPokemon(null);
   };
+
+  const fetchPokemonData = async (pokemonId: number) => {
+    if (pokedex[pokemonId]) {
+      return pokedex[pokemonId];
+    }
+
+    try {
+      const response = await fetch(`${links.pokeApiBaseUrl}${pokemonId}`);
+      const data = await response.json();
+      pokedex[pokemonId] = data;
+      console.log("Fetched Pokémon data:", data);
+      return data;
+    } catch (error) {
+      console.error("Error fetching Pokémon data:", error);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    fetchPokemonData(1);
+  });
 
   return (
     <>
