44ade33d175d287c0f71f06ea4c7897a027023a0
Mark <markfitz815@gmail.com>
2026-03-04 21:22:36 +0000

    Add image and pokemon to PokeInfo

--- src/components/PokeInfo/PokeInfo..tsx
+++ src/components/PokeInfo/PokeInfo..tsx
@@ -1,6 +1,18 @@
+import Styles from "./PokeInfo.module.css";
+
 function PokeInfo({ currMonData }: { currMonData: any }) {
   return (
-    <div className="poke-info">PokeInfo for Pokémon ID: {currMonData?.id}</div>
+    <div className={Styles["poke-info"]}>
+      <img
+        className={Styles["poke-image"]}
+        src={currMonData?.sprites?.other["official-artwork"].front_default}
+        alt={`${currMonData?.name} official artwork`}
+      />
+      <h2 className={Styles["poke-name"]}>
+        {`${currMonData?.name} (#${currMonData?.id})`}
+      </h2>
+      <p>Type: {currMonData?.type?.join(", ")}</p>
+    </div>
   );
 }
 
